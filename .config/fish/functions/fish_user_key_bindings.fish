5f18f6552e647e48b5f8d2fab1d90d3c16a534f5
Jeremiah Menétrey <superjun1@gmail.com>
2022-02-12 18:38:23 +0100

    Move key bindings to standard location

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,7 @@
 function fish_user_key_bindings --description "User key bindings"
+    # ALT+X: Toggle private mode
     bind --user \ex 'if set -q fish_private_mode; exec fish; else; exec fish --private; end'
+
+    # CTRL+ALT+L: Clear screen like real men do.
+    bind --user \e\cl 'clear; commandline -f repaint'
 end
