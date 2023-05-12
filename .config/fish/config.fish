cc29f78816bb18d7463ebcfd6fc1be7508dc9aa1
Jeremiah Menétrey <superjun1@gmail.com>
2023-05-12 02:16:01 +0200

    Move fish bindings to config.fish
    Makes it easier to find them again.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # ALT+X: Toggle private mode
+    bind --user \ex 'if set -q fish_private_mode; exec fish; else; exec fish --private; end'
+
+    # CTRL+ALT+L: Clear screen like real men do.
+    bind --user \e\cl 'clear; commandline -f repaint'
 end
