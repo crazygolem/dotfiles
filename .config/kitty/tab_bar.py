0cbaf93f7d7dd04c1506cbba24ae116a83efd8bd
Jeremiah Menétrey <superjun1@gmail.com>
2024-09-16 20:54:34 +0200

    Streamline draw_tab logic
    The logic that came from kitty's draw_tab_with_powerline was too convoluted,
    with parts I didn't manage to grasp. It should now be pretty easy to follow.
    
    This change improves the drawing of the first and last tabs (the title gets
    drawn closer to the edge), and fixes the last tab being too wide (due to the
    space reserved for a separator that doesn't get drawn).

--- .config/kitty/tab_bar.py
+++ .config/kitty/tab_bar.py
@@ -40,20 +40,22 @@ def num_tabs(tab_id: int):
             return len(tm.tabs)
     return 1
 
-# Computes the tab length such that all tabs have approx. the same size and the
-# tab bar is filled.
-def tab_length(
+# Computes the title length such that all tabs have about the same size to
+# display the title, ensuring that the tab bar is filled exactly.
+def title_length(
     screen: Screen,
     tab_id: int,
     index: int, # 1-based
+    sep_width: int,
 ) -> int:
     ntabs = num_tabs(tab_id)
-    ncols = screen.columns
+    ncols = screen.columns - (ntabs - 1) * sep_width
 
     width = ncols // ntabs
-    # compensate missing width m by adding 1 to m first tabs
+    # Compensate missing width m by adding 1 to first m tabs. There is always at
+    # least m tabs (pigeonhole principle).
     width += 1 if (index <= ncols % ntabs) else 0
-    return width - 1 # somehow length is 0-based
+    return width
 
 # Splits a string at length `n`, considering wide charactes that display on two
 # cells as being of length 2.
@@ -73,6 +75,27 @@ def wsplit(s: str, n: int) -> tuple[str, str]:
         first += c
 
     return (first, last)
+
+def cfg_separator(
+    draw_data: DrawData,
+    screen: Screen,
+    extra_data: ExtraData
+) -> tuple[str, bool]:
+    sep_cfg = get_options().tab_separator
+    sep_hard, sep_soft = separator_symbols.get(sep_cfg) or (
+        ('▌', sep_cfg) if (l := wcswidth(sep_cfg)) == 1
+        else wsplit(sep_cfg, l // 2) if l % 2 == 0
+        else separator_symbols.get('simple')
+    )
+
+    # Checking using colors instead of tab.is_active, because users could choose
+    # to highlight active tab in other ways, and the soft/hard distinction for
+    # separators is only a colors thing.
+    tab_bg = screen.cursor.bg
+    next_tab_bg = as_rgb(draw_data.tab_bg(extra_data.next_tab)) if extra_data.next_tab else None
+    sep = sep_hard if next_tab_bg and next_tab_bg != tab_bg else sep_soft
+
+    return (sep is sep_hard, sep, wcswidth(sep), next_tab_bg)
 
 # Wrapper for Kitty's `draw_title` function that aligns the status zone and the
 # title correctly, making sure to preserve the expected tab size.
@@ -106,11 +129,17 @@ def draw_title(
         active_title_template = make_title(draw_data.active_title_template),
     )
 
+    before = screen.cursor.x
     kitty_draw_title(draw_data, screen, tab, index, max_title_length)
+    extra = screen.cursor.x - before - max_title_length
+    if extra > 0 and extra + 1 < screen.cursor.x:
+        screen.cursor.x -= extra + 1
+        screen.draw('…')
+
 
 
 # Draws the tab bar as a fullwidth bar with tabs of equal size.
-# Based on Kitty's `draw_tab_with_powerline`.
+# Inspired by Kitty's `draw_tab_with_powerline`.
 def draw_tab(
     draw_data: DrawData,
     screen: Screen,
@@ -121,8 +150,14 @@ def draw_tab(
     is_last: bool,
     extra_data: ExtraData
 ) -> int:
+    (sep_is_hard, sep, sep_width, next_tab_bg) = cfg_separator(
+        draw_data, screen, extra_data)
+
     # Override kitty's tab length algorithm. We treat this as fixed length.
-    max_tab_length = tab_length(screen, tab.tab_id, index)
+    max_title_length = title_length(screen, tab.tab_id, index, sep_width)
+    max_tab_length = max_title_length + (sep_width if not is_last else 0)
+
+    # Early exit for layout-only call
     if extra_data.for_layout:
         screen.cursor.x += max_tab_length
         return screen.cursor.x
@@ -131,52 +166,27 @@ def draw_tab(
     tab_fg = screen.cursor.fg
     default_bg = as_rgb(int(draw_data.default_bg))
 
-    if extra_data.next_tab:
-        next_tab_bg = as_rgb(draw_data.tab_bg(extra_data.next_tab))
-        needs_soft_separator = next_tab_bg == tab_bg
-    else:
-        next_tab_bg = default_bg
-        needs_soft_separator = False
-
-    sep = get_options().tab_separator
-    separator_symbol, soft_separator_symbol = separator_symbols.get(sep) or (
-        ('▌', sep) if (l := wcswidth(sep)) == 1
-        else wsplit(sep, l // 2) if l % 2 == 0
-        else separator_symbols.get('simple')
-    )
-
-    min_title_length = 1 + 2
-    start_draw = 2
-
-    if screen.cursor.x == 0:
-        screen.cursor.bg = tab_bg
-        screen.draw(' ')
-        start_draw = 1
 
     screen.cursor.bg = tab_bg
-    if min_title_length >= max_tab_length:
-        screen.draw('…')
-    else:
-        draw_title(draw_data, screen, tab, index, max_tab_length - 2)
-        extra = screen.cursor.x + start_draw - before - max_tab_length
-        if extra > 0 and extra + 1 < screen.cursor.x:
-            screen.cursor.x -= extra + 1
-            screen.draw('…')
 
-    if not needs_soft_separator:
+    if max_title_length <= 3:
+        screen.draw(' … ')
+    else:
         screen.draw(' ')
-        if is_last:
-            # Replaces the separator so it fits the edge of the screen
-            screen.draw(' ')
-            # Should not happen as we increase tab width such that the bar gets
-            # completely filled, but if anything weird happens we cover past the
-            # last tab so that the "inactive" background doesn't show.
-            if (e := screen.columns - screen.cursor.x) > 0:
-                screen.draw(' ' * e)
-        else:
-            screen.cursor.fg = tab_bg
-            screen.cursor.bg = next_tab_bg
-            screen.draw(separator_symbol)
+        draw_title(draw_data, screen, tab, index, max_title_length - 2)
+        screen.draw(' ')
+
+    if is_last:
+        # Should not happen as we compute tab width such that the tab bar gets
+        # completely filled, but if anything weird happens we cover past the
+        # last tab so that the bar's background doesn't show (esp. if the last
+        # tab is active).
+        if (e := screen.columns - screen.cursor.x) > 0:
+            screen.draw(' ' * e)
+    elif sep_is_hard:
+        screen.cursor.fg = tab_bg
+        screen.cursor.bg = next_tab_bg
+        screen.draw(sep)
     else:
         prev_fg = screen.cursor.fg
         if tab_bg == tab_fg:
@@ -186,10 +196,7 @@ def draw_tab(
             c2 = draw_data.inactive_bg.contrast(draw_data.inactive_fg)
             if c1 < c2:
                 screen.cursor.fg = default_bg
-        screen.draw(f' {soft_separator_symbol}')
+        screen.draw(sep)
         screen.cursor.fg = prev_fg
 
-    end = screen.cursor.x
-    if end < screen.columns:
-        screen.draw(' ')
-    return end
+    return screen.cursor.x
